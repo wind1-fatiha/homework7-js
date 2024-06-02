0d22f56bf6942b636ed15367faed517dc8f580d8
wind1-fatiha <talaybekovafatiha08006@gmail.com>
2024-06-02 18:42:33 +0600

    first commit

---
+++ dz.js
@@ -0,0 +1,5 @@
+const array=[1,2,23,44,66,22,23]
+const umnojenie=array.map(function (item) {
+ return item*2
+});
+console.log(umnojenie);
---
+++ main.js
@@ -0,0 +1,32 @@
+// const array=[1,2,3,4,5,6,7,80,78]
+
+// const obg={
+//     getName:()=>{
+
+//     },
+//     getAge:function (p) {
+        
+//     }
+// }
+
+// obg.getAge()
+
+// const areArray=array.filter((el)=>3)
+// const sorted=array.sort((a,b)=>b-a)
+// const chancged=array.map((item)=>item+10)
+// const finded=array.find((el)=>el===5)
+// const filled = array.fill(6)
+
+// const strings = ["hello", "world", "javascript", "is", "fun"]
+
+// const users = [
+//     { name: "Alice", age: 25 },
+//     { name: "Bob", age: 30 },
+//     { name: "Charlie", age: 35 }
+//     { name: "Alice", age: 25 },
+//     { name: "Bob", age: 30 },
+//     { name: "Charlie", age: 35 }
+//   ];
+// console.log(users.find((item)));
+
+
